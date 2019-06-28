7fdae98d80165481882d8ebb53bd8a013e40f121
Nickolas Gamblin <nickgamblin@gmail.com>
2019-06-27 23:30:35 -0500

    working model and controllers for dog

--- controllers/dogs.js
+++ controllers/dogs.js
@@ -5,10 +5,37 @@ var router = express.Router();
 var dog = require('../models/dog');
 var orm = require('../config/orm');
 
-router.get('/api/dogs', function(req,res){
-    dog.allGoodDogs(function(error, dog_data){
+router.get('/api/dogs', function (req, res) {
+    dog.allGoodDogs(function (error, dog_data) {
         res.json(dog_data);
     })
-})
+});
+
+router.get('/api/dogs/:id', function (req, res) {
+    var id = req.params.id
+    dog.dogById(id, function (error, dog_data) {
+        res.json(dog_data);
+    })
+});
+
+router.post('/api/dogs', function (req, res) {
+    var newDog = {
+    //Place Holder Dog // to be deleted
+        dog_id: 3,
+        dog_name: "Brett",
+        dog_breed: "Shib",
+        dog_age: 5,
+        dog_img_url: "www.google.com",
+        dog_blurb: "Goodest Boi"
+    }
+    dog.insertDog(newDog, function (error, dog_data) {
+        if (error) {
+            res.json(error)
+        } else {
+            res.json(dog_data);
+        }
+    })
+});
+
 
 module.exports = router;
--- models/dog.js
+++ models/dog.js
@@ -1,14 +1,29 @@
 var orm = require('../config/orm')
 
 var dog = {
-    allGoodDogs: function(callback){
+    allGoodDogs: function (callback) {
         let query = {
             table: 'dogs'
         };
         orm.select(query, callback);
     },
 
-    
+    insertDog: function (newDog, callback) {
+        let query = {
+            table: 'dogs',
+            data: newDog,
+            // debug: true
+        };
+        orm.insert(query, callback);
+    },
+
+    dogById: function (id, callback){
+        let query = {
+            table: 'dogs',
+            where: [{dog_id: id}]
+        };
+        orm.select(query, callback);
+    }
 };
 
 module.exports = dog;
