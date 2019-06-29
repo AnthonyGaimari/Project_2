483bdb79cb6529a66d3ef8bfbd8f621d6463edb1
Nickolas Gamblin <nickgamblin@gmail.com>
2019-06-29 12:29:26 -0500

    Ticket#22

---
+++ controllers/routes.js
@@ -0,0 +1,77 @@
+//Routes
+
+
+var express = require('express');
+var router = express.Router();
+var dog = require('../models/dog');
+var user = require('../models/user');
+
+var orm = require('../config/orm');
+
+//Get all dogs
+router.get('/api/dogs', function (req, res) {
+    dog.allGoodDogs(function (error, dog_data) {
+        res.json(dog_data);
+    })
+});
+
+//Get dog by ID
+router.get('/api/dogs/:id', function (req, res) {
+    var id = req.params.id
+    dog.dogById(id, function (error, dog_data) {
+        res.json(dog_data);
+    })
+});
+
+//Add a new dog
+router.post('/api/dogs', function (req, res) {
+    var newDog = {
+    //Place Holder Dog // to be deleted
+        dog_name: "Dog1",
+        dog_breed: "Shib",
+        dog_age: 5,
+        dog_img_url: "www.google.com",
+        dog_blurb: "Goodest Boi"
+    }
+    dog.insertDog(newDog, function (error, dog_data) {
+        if (error) {
+            res.json(error)
+        } else {
+            res.json(dog_data);
+        }
+    })
+});
+
+//USERS
+
+router.post('/api/users', function (req, res) {
+    var newUser = {
+    //Place Holder User // to be deleted
+        user_id: 1,
+        username: "Nick",
+        user_email: "nick@gamblin.com"
+    }
+    user.insertUser(newUser, function (error, user_data) {
+        if (error) {
+            res.json(error)
+        } else {
+            res.json(user_data);
+        }
+    })
+});
+
+router.post('/api/user/login', function (req, res) {
+    // var username = req.body.username
+    // var password = req.body.password
+    var username = "Nickolas"
+    var password = "p#ssword"
+    user.userByUsername(username, function (error, user_data) {
+    user_data = user_data[0];
+    if (user_data.password === password){
+        res.status(200)
+    } else
+        res.status(400).json({'error': 'Incorrect username or password'});       
+    })
+
+});
+module.exports = router;
